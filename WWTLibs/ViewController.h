fd143ef29d0dea8989625355a8294859a2619f6c
wuwentao <wuwentao@qiyi.com>
2017-07-30 21:14:37 +0800

    Progect init

---
+++ WWTLibs/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WWTLibs
+//
+//  Created by wuwentao on 17/7/30.
+//  Copyright © 2017年 wuwentao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WWTLibs/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WWTLibs
+//
+//  Created by wuwentao on 17/7/30.
+//  Copyright © 2017年 wuwentao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
